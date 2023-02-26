beaaa250a0e6f6ff4896ed73dd0b7f4790986e89
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2023-02-26 15:24:26 -0600

    Removed startup dependency on AjaxMin

--- SEBrowser/Startup.cs
+++ SEBrowser/Startup.cs
@@ -69,17 +69,10 @@ public class Startup
     }
 
     private static readonly AuthenticationOptions s_authenticationOptions;
-    private static Dictionary<string, Assembly> s_assemblyCache;
-    private static bool s_addedResolver;
 
     static Startup()
     {
-        // Steps to properly load desired version of AjaxMin (prevents version conflict between GSF and WebGrease):
-        //   1) Reference new GSF version of AjaxMin via NuGet
-        //   2) Set AjaxMin reference property "Copy Local" to false
-        //   3) Add AjaxMin to project as an embedded resource
-        //   4) Load AjaxMin into AppDomain from embedded resource
-        LoadAssemblyFromResource("AjaxMin");
+
         SetupTempPath();
 
         s_authenticationOptions = new AuthenticationOptions
@@ -102,56 +95,6 @@ public class Startup
         Type type = typeof(T);
         object instance = type.Assembly.CreateInstance(type.FullName!, false, BindingFlags.Instance | BindingFlags.NonPublic, null, args, null, null);
         return (T)instance;
-    }
-
-    private static void LoadAssemblyFromResource(string assemblyName)
-    {
-        if (!s_addedResolver)
-        {
-            // Hook into assembly resolve event for current domain so it can load assembly from embedded resource
-            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblyFromResource;
-            s_addedResolver = true;
-        }
-
-        // Load the assembly (this will invoke event that will resolve assembly from resource)
-        AppDomain.CurrentDomain.Load(assemblyName);
-    }
-
-    private static Assembly ResolveAssemblyFromResource(object sender, ResolveEventArgs e)
-    {
-        string shortName = e.Name.Split(',')[0];
-
-        if ((s_assemblyCache ??= new Dictionary<string, Assembly>()).TryGetValue(shortName, out Assembly resourceAssembly) && resourceAssembly is not null)
-            return resourceAssembly;
-
-        Assembly entryAssembly = typeof(Startup).Assembly;
-
-        // Loops through all of the resources in the executing assembly
-        foreach (string name in entryAssembly.GetManifestResourceNames())
-        {
-            // See if the embedded resource name matches the assembly trying to be loaded
-            if (string.Compare(FilePath.GetFileNameWithoutExtension(name), $"{nameof(SEBrowser)}.{shortName}", StringComparison.OrdinalIgnoreCase) != 0)
-                continue;
-
-            // If so, load embedded resource assembly into a binary buffer
-            Stream resourceStream = entryAssembly.GetManifestResourceStream(name);
-
-            if (resourceStream is null)
-                continue;
-
-            byte[] buffer = new byte[resourceStream.Length];
-            resourceStream.Read(buffer, 0, (int)resourceStream.Length);
-            resourceStream.Close();
-
-            // Load assembly from binary buffer
-            resourceAssembly = Assembly.Load(buffer);
-
-            // Add assembly to the cache
-            s_assemblyCache.Add(shortName, resourceAssembly);
-            break;
-        }
-
-        return resourceAssembly;
     }
 
     private static void SetupTempPath()
