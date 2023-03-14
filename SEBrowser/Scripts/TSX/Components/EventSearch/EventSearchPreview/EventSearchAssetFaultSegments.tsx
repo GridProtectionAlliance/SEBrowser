44a1958895a53071e0cea529bd6fe347fc4af493
adambhagemeyer <adam.hagemeyer00@gmail.com>
2023-03-14 15:28:03 -0400

    Specified IWidget type

--- SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/EventSearchAssetFaultSegments.tsx
+++ SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/EventSearchAssetFaultSegments.tsx
@@ -26,7 +26,7 @@ import SEBrowserService from './../../../../TS/Services/SEBrowser';
 import moment from 'moment';
 import { SEBrowser } from '../../../global';
 
-const EventSearchAssetFaultSegments: React.FC<SEBrowser.IWidget> = (props) => {
+const EventSearchAssetFaultSegments: React.FC<SEBrowser.IWidget<any>> = (props) => {
 
     const [tableRows, setTableRows] = React.useState<JSX.Element[]>([]);
     const [count, setCount] = React.useState<number>(0);
