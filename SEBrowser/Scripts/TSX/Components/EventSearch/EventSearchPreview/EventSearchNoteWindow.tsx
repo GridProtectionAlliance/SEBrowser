77bdcc3eacb6651c4a3753b5a57e48d7d545e41a
Gary Pinkley <gary.pinkley@icloud.com>
2023-07-11 14:58:18 -0400

    Updating category to be types in EventSearchNoteWindow at line 212

--- SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/EventSearchNoteWindow.tsx
+++ SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/EventSearchNoteWindow.tsx
@@ -209,7 +209,7 @@ const NoteWidget: React.FC<SEBrowser.IWidget<ISetting>> = (props) => {
                     ShowCard={false}
                     Filter={(n) => selectedTags.find(i => i == n.NoteTagID) != null}
                 /> : <div className={'alert alert-warning'}>
-                    <p>At least 1 Category needs to be selected.</p>
+                    <p>At least 1 Type needs to be selected.</p>
                 </div>}
             </div>
         </div>
