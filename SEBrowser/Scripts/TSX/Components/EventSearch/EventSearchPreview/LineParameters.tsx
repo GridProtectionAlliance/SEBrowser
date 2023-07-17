23429bf97cd4fe5e8157bb8720bd8517193c7767
Gary Pinkley <gary.pinkley@icloud.com>
2023-07-17 16:01:35 -0400

    Added interfaces ILineParameters and ILoopImpedance for legibility, type, and maintenance improvement

--- SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/LineParameters.tsx
+++ SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/LineParameters.tsx
@@ -25,6 +25,26 @@ import React from 'react';
 import { SEBrowser } from '../../../global';
 import Table from '@gpa-gemstone/react-table';
 
+interface ILineParameters {
+    ID?: number,
+    Length?: number,
+    X0?: number,
+    X1?: number,
+    R1?: number,
+    R0?: number
+}
+
+interface ILoopImpedance {
+    Length: number,
+    ZS: string,
+    Ang: string,
+    RS: string,
+    XS: string,
+    PerMileZS: string,
+    PerMileRS: string,
+    PerMileXS: string
+}
+
 const LineParameters: React.FC<SEBrowser.IWidget<any>> = (props) => {
     const [hidden, setHidden] = React.useState<boolean>(true);
     const [lineParameters, setLineParameters] = React.useState<{ ID?: number, Length?: number,X0?: number, X1?: number, R1?: number, R0?: number}>(null);
