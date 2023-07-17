e9c01ccea59382ea0dfbeacc6886308f613b000f
Gary Pinkley <gary.pinkley@icloud.com>
2023-07-17 15:39:26 -0400

    Changing single use let to const

--- SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/HECCO/InterruptionReport.tsx
+++ SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/HECCO/InterruptionReport.tsx
@@ -43,7 +43,7 @@ const InterruptionReport: React.FC<SEBrowser.IWidget<any>> = (props) => {
     const [hours, setHours] = React.useState<number>(6);
 
     React.useEffect(() => {
-        let handle = getData();
+        const handle = getData();
         return () => { if (handle != null && handle.abort != null) handle.abort();}
     }, [hours])
 
