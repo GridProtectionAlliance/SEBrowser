c3876eb61e3f41e48cfc7d0f76f58636d02e88c6
Gary Pinkley <gary.pinkley@icloud.com>
2023-07-17 16:02:32 -0400

    Implementing new interface

--- SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/LineParameters.tsx
+++ SEBrowser/Scripts/TSX/Components/EventSearch/EventSearchPreview/LineParameters.tsx
@@ -47,7 +47,7 @@ interface ILoopImpedance {
 
 const LineParameters: React.FC<SEBrowser.IWidget<any>> = (props) => {
     const [hidden, setHidden] = React.useState<boolean>(true);
-    const [lineParameters, setLineParameters] = React.useState<{ ID?: number, Length?: number,X0?: number, X1?: number, R1?: number, R0?: number}>(null);
+    const [lineParameters, setLineParameters] = React.useState<ILineParameters>(null);
     React.useEffect(() => {
         return GetData();
     }, [props.eventID]);
