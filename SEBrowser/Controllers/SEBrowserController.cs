fe911ce78c1177128691bfaeb695f27fc5172f01
Gabriel Santos <102973566+gcsantos-gpa@users.noreply.github.com>
2023-08-07 15:53:30 -0400

    Update SEBrowser/Controllers/SEBrowserController.cs
    Co-authored-by: Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>

--- SEBrowser/Controllers/SEBrowserController.cs
+++ SEBrowser/Controllers/SEBrowserController.cs
@@ -65,7 +65,8 @@ namespace SEBrowser.Controllers
             using (AdoDataConnection connection = new AdoDataConnection(SettingsCategory))
             {
                 // Seperate setting from system setting due to UNIX/Windows differences, since Windows does not use the IANA timezones, and js requires them
-                string systemTime = connection.ExecuteScalar<string>("SELECT TOP 1 [Value] FROM [SEBrowser.Setting] WHERE Name = 'Timezone'");
+                string systemTime = connection.ExecuteScalar<string>("SELECT TOP 1 [Value] FROM [SEBrowser.Setting] WHERE Name = 'System.IANATimeZone'");
+
                 if (string.IsNullOrEmpty(systemTime))
                     return Ok(0);
                 return Ok(systemTime);
