05796372a9cb00e0ca4c9d36295b56d034f874eb
prestoncraw <prestoncrawford530@gmail.com>
2024-03-11 15:21:31 -0400

    Moved StandarMagDurCurve to openXDA modelController

--- SEBrowser/Scripts/TSX/Store.ts
+++ SEBrowser/Scripts/TSX/Store.ts
@@ -34,7 +34,7 @@ declare let homePath: string;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>
 
-export const MagDurCurveSlice = new GenericSlice<OpenXDA.Types.MagDurCurve>('MagDurCurve', `${homePath}api/StandardMagDurCurve`, 'Name');
+export const MagDurCurveSlice = new GenericSlice<OpenXDA.Types.MagDurCurve>('MagDurCurve', `${homePath}api/openXDA/StandardMagDurCurve`, 'Name');
 export const AssetGroupSlice = new GenericSlice<OpenXDA.Types.AssetGroup>('AssetGroup', `${homePath}api/openXDA/AssetGroup`, 'Name');
 
 export const MeterSlice = new GenericSlice<SystemCenter.Types.DetailedMeter>("Meter", `${homePath}api/OpenXDA/Meter`, "Name", true);
