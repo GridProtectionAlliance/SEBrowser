cb9dcb66937ae4c44b2c5fbd1b4c06b5b1536474
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-09-03 10:30:28 -0400

    fix lint error on namespace

--- SEBrowser/Scripts/TSX/Components/TrendData/HelperFunctions.ts
+++ SEBrowser/Scripts/TSX/Components/TrendData/HelperFunctions.ts
@@ -47,168 +47,175 @@ export function findCommonComponents(components: string[], channels: TrendSearch
     return allCommon;
 }
 
-export namespace TrendDefaults {
-    const lineSettings = "SEBrowser.TrendSearch.LineSettingsBundle";
-    export function storeLineSettings(bundle: TrendSearch.ILinePlotSettingsBundle): void {
-        localStorage.setItem(lineSettings, JSON.stringify(bundle))
-    }
-    export function getLineSettingsOrDefault(): TrendSearch.ILinePlotSettingsBundle {
-        if (Object.prototype.hasOwnProperty.call(localStorage, lineSettings))
-            return JSON.parse(localStorage.getItem(lineSettings));
-        return {
-            Minimum: {
-                Default: {
-                    Width: 3,
-                    Type: 'short-dash'
-                },
-                ShouldApply: false
+const lineSettings = "SEBrowser.TrendSearch.LineSettingsBundle";
+export function storeLineSettings(bundle: TrendSearch.ILinePlotSettingsBundle): void {
+    localStorage.setItem(lineSettings, JSON.stringify(bundle))
+}
+export function getLineSettingsOrDefault(): TrendSearch.ILinePlotSettingsBundle {
+    if (Object.prototype.hasOwnProperty.call(localStorage, lineSettings))
+        return JSON.parse(localStorage.getItem(lineSettings));
+    return {
+        Minimum: {
+            Default: {
+                Width: 3,
+                Type: 'short-dash'
             },
-            Maximum: {
-                Default: {
-                    Width: 3,
-                    Type: 'short-dash'
-                },
-                ShouldApply: false
+            ShouldApply: false
+        },
+        Maximum: {
+            Default: {
+                Width: 3,
+                Type: 'short-dash'
             },
-            Average: {
-                Default: {
-                    Width: 3,
-                    Type: 'solid'
-                },
-                ShouldApply: false
+            ShouldApply: false
+        },
+        Average: {
+            Default: {
+                Width: 3,
+                Type: 'solid'
             },
-            Colors: {
-                Default: {
-                    ApplyType: "Random",
-                    Colors: [
-                        {
-                            Label: "Color 1",
-                            Minimum: "#A30000",
-                            Average: "#A30000",
-                            Maximum: "#A30000"
-                        },
-                        {
-                            Label: "Color 2",
-                            Minimum: "#0029A3",
-                            Average: "#0029A3",
-                            Maximum: "#0029A3"
-                        },
-                        {
-                            Label: "Color 3",
-                            Minimum: "#007A29",
-                            Average: "#007A29",
-                            Maximum: "#007A29"
-                        },
-                        {
-                            Label: "Color 4",
-                            Minimum: "#FF0000",
-                            Average: "#FF0000",
-                            Maximum: "#FF0000"
-                        },
-                        {
-                            Label: "Color 5",
-                            Minimum: "#0066CC",
-                            Average: "#0066CC",
-                            Maximum: "#0066CC"
-                        },
-                        {
-                            Label: "Color 6",
-                            Minimum: "#33CC33",
-                            Average: "#33CC33",
-                            Maximum: "#33CC33"
-                        }
-                    ]
-                },
-                ShouldApply: false
-            }
+            ShouldApply: false
+        },
+        Colors: {
+            Default: {
+                ApplyType: "Random",
+                Colors: [
+                    {
+                        Label: "Color 1",
+                        Minimum: "#A30000",
+                        Average: "#A30000",
+                        Maximum: "#A30000"
+                    },
+                    {
+                        Label: "Color 2",
+                        Minimum: "#0029A3",
+                        Average: "#0029A3",
+                        Maximum: "#0029A3"
+                    },
+                    {
+                        Label: "Color 3",
+                        Minimum: "#007A29",
+                        Average: "#007A29",
+                        Maximum: "#007A29"
+                    },
+                    {
+                        Label: "Color 4",
+                        Minimum: "#FF0000",
+                        Average: "#FF0000",
+                        Maximum: "#FF0000"
+                    },
+                    {
+                        Label: "Color 5",
+                        Minimum: "#0066CC",
+                        Average: "#0066CC",
+                        Maximum: "#0066CC"
+                    },
+                    {
+                        Label: "Color 6",
+                        Minimum: "#33CC33",
+                        Average: "#33CC33",
+                        Maximum: "#33CC33"
+                    }
+                ]
+            },
+            ShouldApply: false
         }
     }
+}
 
-    const markerSettings = "SEBrowser.TrendSearch.MarkerSettingsBundle";
-    export function storeMarkerSettings(bundle: TrendSearch.IMarkerSettingsBundle): void {
-        localStorage.setItem(markerSettings, JSON.stringify(bundle))
-    }
-    export function getMarkerSettingsOrDefault(): TrendSearch.IMarkerSettingsBundle {
-        if (Object.prototype.hasOwnProperty.call(localStorage, markerSettings))
-            return JSON.parse(localStorage.getItem(markerSettings));
-        return {
-            Symb: {
-                Default: {
-                    // Need to overwrite these
-                    ID: "Symb",
-                    xPos: undefined,
-                    yPos: undefined,
-                    axis: "left",
-                    xBox: undefined,
-                    yBox: undefined,
-                    // Symbol
-                    symbol: SVGIcons.ArrowDropDown,
-                    radius: 12,
-                    color: "#000000",
-                    // Note
-                    format: "HH:mm",
-                    note: "",
-                    opacity: 1,
-                    fontColor: "#000000",
-                    fontSize: 1,
-                    type: "Symb"
-                },
-                ShouldApply: false
+const markerSettings = "SEBrowser.TrendSearch.MarkerSettingsBundle";
+export function storeMarkerSettings(bundle: TrendSearch.IMarkerSettingsBundle): void {
+    localStorage.setItem(markerSettings, JSON.stringify(bundle))
+}
+export function getMarkerSettingsOrDefault(): TrendSearch.IMarkerSettingsBundle {
+    if (Object.prototype.hasOwnProperty.call(localStorage, markerSettings))
+        return JSON.parse(localStorage.getItem(markerSettings));
+    return {
+        Symb: {
+            Default: {
+                // Need to overwrite these
+                ID: "Symb",
+                xPos: undefined,
+                yPos: undefined,
+                axis: "left",
+                xBox: undefined,
+                yBox: undefined,
+                // Symbol
+                symbol: SVGIcons.ArrowDropDown,
+                radius: 12,
+                color: "#000000",
+                // Note
+                format: "HH:mm",
+                note: "",
+                opacity: 1,
+                fontColor: "#000000",
+                fontSize: 1,
+                type: "Symb"
             },
-            VeHo: {
-                Default: {
-                    // Need to overwrite these
-                    ID: "Veho",
-                    value: undefined,
-                    axis: "left",
-                    isHori: undefined,
-                    // Defaults
-                    color: "#E41000",
-                    line: "short-dash",
-                    width: 4,
-                    type: "VeHo"
-                },
-                ShouldApply: false
+            ShouldApply: false
+        },
+        VeHo: {
+            Default: {
+                // Need to overwrite these
+                ID: "Veho",
+                value: undefined,
+                axis: "left",
+                isHori: undefined,
+                // Defaults
+                color: "#E41000",
+                line: "short-dash",
+                width: 4,
+                type: "VeHo"
             },
-            Event: {
-                Default: {
-                    // Need to overwrite ID
-                    ID: "Event",
-                    axis: "left",
-                    type: "Event-Vert",
-                    color: "#E41000",
-                    line: "short-dash",
-                    width: 4
-                },
-                ShouldApply: false
-            }
+            ShouldApply: false
+        },
+        Event: {
+            Default: {
+                // Need to overwrite ID
+                ID: "Event",
+                axis: "left",
+                type: "Event-Vert",
+                color: "#E41000",
+                line: "short-dash",
+                width: 4
+            },
+            ShouldApply: false
         }
     }
+}
 
-    const plotSettings = "SEBrowser.TrendSearch.PlotSettings";
-    const storeFields: (keyof TrendSearch.ITrendPlot)[] = ["LabelComponents", "Height", "Width", "Metric", "ShowEvents"];
-    export function storePlotSettings(plot: TrendSearch.ITrendPlot): void {
-        const storedItems: { [key: string]: unknown } = {};
-        storeFields.forEach(field => storedItems[field] = plot[field]);
-        localStorage.setItem(plotSettings, JSON.stringify(storedItems));
-    }
-    export function getPlotSettingsOrDefault(): TrendSearch.ITrendPlot {
-        const defaultPlot: TrendSearch.ITrendPlot = {
-            TimeFilter: { date: moment.utc().format(momentDateFormat), time: '12:00:00.000', windowSize: 12, timeWindowUnits: 3 },
-            Type: 'Line',
-            Channels: [],
-            PlotFilter: [{ Label: "Minimum", Value: "Minimum", Selected: true }, { Label: "Maximum", Value: "Maximum", Selected: true }, { Label: "Average/Values", Value: "Average", Selected: true }],
-            ID: "blank",
-            Width: 50,
-            Height: 50,
-            AxisZoom: 'AutoValue',
-            ShowEvents: false,
-            LabelComponents: []
-        };
-        let storedItems: { [key: string]: unknown } = {};
-        if (Object.prototype.hasOwnProperty.call(localStorage, plotSettings))
-            storedItems = JSON.parse(localStorage.getItem(plotSettings));
-        Object.keys(storedItems).forEach(field => defaultPlot[field] = storedItems[field]);
-        return defaultPlot;
-    }
+const plotSettings = "SEBrowser.TrendSearch.PlotSettings";
+const storeFields: (keyof TrendSearch.ITrendPlot)[] = ["LabelComponents", "Height", "Width", "Metric", "ShowEvents"];
+export function storePlotSettings(plot: TrendSearch.ITrendPlot): void {
+    const storedItems: { [key: string]: unknown } = {};
+    storeFields.forEach(field => storedItems[field] = plot[field]);
+    localStorage.setItem(plotSettings, JSON.stringify(storedItems));
+}
+export function getPlotSettingsOrDefault(): TrendSearch.ITrendPlot {
+    const defaultPlot: TrendSearch.ITrendPlot = {
+        TimeFilter: { date: moment.utc().format(momentDateFormat), time: '12:00:00.000', windowSize: 12, timeWindowUnits: 3 },
+        Type: 'Line',
+        Channels: [],
+        PlotFilter: [{ Label: "Minimum", Value: "Minimum", Selected: true }, { Label: "Maximum", Value: "Maximum", Selected: true }, { Label: "Average/Values", Value: "Average", Selected: true }],
+        ID: "blank",
+        Width: 50,
+        Height: 50,
+        AxisZoom: 'AutoValue',
+        ShowEvents: false,
+        LabelComponents: []
+    };
+    let storedItems: { [key: string]: unknown } = {};
+    if (Object.prototype.hasOwnProperty.call(localStorage, plotSettings))
+        storedItems = JSON.parse(localStorage.getItem(plotSettings));
+    Object.keys(storedItems).forEach(field => defaultPlot[field] = storedItems[field]);
+    return defaultPlot;
+}
+
+export const TrendDefaults = {
+    storeLineSettings,
+    getLineSettingsOrDefault,
+    storeMarkerSettings,
+    getMarkerSettingsOrDefault,
+    storePlotSettings,
+    getPlotSettingsOrDefault
 } 
