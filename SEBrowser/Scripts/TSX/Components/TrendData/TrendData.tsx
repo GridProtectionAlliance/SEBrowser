2b8ff0862109363b36f6ee5fe04b08d9dee06362
prestoncraw <prestoncrawford530@gmail.com>
2026-07-15 20:32:06 -0400

    Fix scroll

--- SEBrowser/Scripts/TSX/Components/TrendData/TrendData.tsx
+++ SEBrowser/Scripts/TSX/Components/TrendData/TrendData.tsx
@@ -163,7 +163,7 @@ const TrendData = () => {
             </ErrorBoundary>
             <ErrorBoundary ErrorMessage={'Error loading page.'} ClassName={'h-100'}>
                 <div className={'row'} style={{ flex: 1, overflow: 'hidden' }}>
-                    <div className={'col-12'} style={{ height: '100%', overflowY: 'scroll', overflowX: 'hidden' }} id={trendSearchId}>
+                    <div className={'col-12'} style={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }} id={trendSearchId}>
                         {plotList.map(element =>
                             <TrendPlot
                                 key={element.ID}
